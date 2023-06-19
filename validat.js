bd4e092331bbddf527482092ad84782d7e253e5a
Sadia Bader <sadiabader433@gmail.com>
2023-06-19 16:03:03 +0500

    file changes

--- validat.js
+++ validat.js
@@ -1,26 +1,36 @@
-let login = () => {
+
+    function validateForm() {
+    let fname = document.forms["registrationForm"]["fname"].value;
+    let lname = document.forms["registrationForm"]["lname"].value;
+    let password = document.forms["registrationForm"]["pass"].value;
+ 
+
+
+    // regExp
+    let fnameRegex = /^[a-zA-Z\s]+$/;
+    let lnameRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+    // let fnameRegex = /^[a-z]{3,10}$/i
+    // let lnameRegex = /^[a-z]{3,10}$/i
+    // let passwordRegex = /^[\w@-]{8,12}$/i
     
-    let fname = document.getElementById('fname').value
-    let lname = document.getElementById('lname').value
-    let password = document.getElementById('pass').value
-    let contact = document.getElementById('contact').value
 
+    if (!fname.match(fnameRegex)) {
+        alert("Please enter a valid fname.");
+        return false;
+      }
+    
+      if (lname.match(lnameRegex)) {
+        alert("Please enter a valid lname.");
+        return false;
+      }
+    
+      if (!password.match(passwordRegex)) {
+        alert("Please enter a valid password. It must contain at least 8 characters, including one uppercase letter, one lowercase letter, one digit, and one special character (!@#$%^&*).");
+        return false;
+      }
+
+     
+     return true;
     }
-function validation(){
-    let str = "A-Z||a-z||0-9"
-    let fname = /^[a-z]{3,10}$/i
-    let lname = /^[a-z]{3,10}$/i
-    let password = /^[\w@-]{8,12}$/i
-    let contact = /^[\d{10}]$/
 
-let result1 = str.match(fname)
-let result2 = str.match(lname)
-let result3 = str.match(password)
-let result4 = str.match(contact)
-console.log(result1)
-console.log(result2)
-console.log(result3)
-console.log(result4)
-}
-
-validation()
